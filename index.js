f467e6e288c11167b8f329404dbbf6a2ec0c1265
Matheus Paulino <128553329+dasilvamatheusp@users.noreply.github.com>
2023-05-16 18:20:54 -0300

    desktop

--- index.js
+++ index.js
@@ -1,39 +1,12 @@
-const questions = document.querySelectorAll('.questions')
 const allQuestions = document.querySelectorAll('.accordion-item')
-const arrow = document.querySelectorAll('.icon')
-let selectedQuestion;
-let selectedResponse;
-let selectedIcon;
 
-  for (let doubt of allQuestions) {
-    doubt.addEventListener('click', e => {
-      const question = doubt.querySelector('.questions')
-      const response = doubt.querySelector('.hidden')
-      const arrowIcon = doubt.querySelector('.icon')
-
-      for (let selectedBold of questions) {
-        selectedBold.classList.remove('question-bold')
-      }
-
-      for (let icon of arrow) {
-        icon.classList.remove('rotate')
-      }
-
-      if (selectedIcon) {
-        selectedIcon.classList.remove('rotate')
-      }
-
-      if (selectedQuestion) {
-        selectedQuestion.classList.remove('question-bold')
-        selectedResponse.classList.add('hidden')
-      }
-
-      arrowIcon.classList.toggle('rotate')
-      question.classList.add('question-bold')
-      response.classList.remove('hidden')
-      
-      selectedIcon = arrowIcon
-      selectedQuestion = question
-      selectedResponse = response
-    })
-  } 
+for (let doubt of allQuestions) {
+  const question = doubt.querySelector('.questions')
+  question.addEventListener('click', ()=>{
+    question.classList.toggle('question-bold')
+    const response = doubt.querySelector('.response')
+    response.classList.toggle('hidden')
+    const arrowIcon = doubt.querySelector('.icon')
+    arrowIcon.classList.toggle('rotate')
+  })
+}
